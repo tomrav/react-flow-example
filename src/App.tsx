398629b9bfea02d2a26f4ede5a364a20941c76e7
Tom Raviv <tomr@wix.com>
2023-10-19 16:24:03 +0300

    Codux and React Flow example

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Flow from "./Flow";
+
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Flow />
+    </div>
+  );
+}
---
+++ src/_codux/boards/app/app.board.tsx
@@ -0,0 +1,12 @@
+import { createBoard } from '@wixc3/react-board';
+import App from '../../../App';
+
+export default createBoard({
+    name: 'App',
+    Board: () => <App />,
+    isSnippet: true,
+    environmentProps: {
+        canvasWidth: 664,
+        canvasHeight: 466
+    }
+});
